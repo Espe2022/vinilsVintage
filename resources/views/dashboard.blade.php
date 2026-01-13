1370b861cc140f235d297cae77b54a55cfae0ee9
Espe <esmgana70@gmail.com>
2026-01-13 14:12:50 +0100

    Modificar gráfico de Dashboard poor Histórico de Productos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,18 +10,49 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-beige-crema border-b border-gray-200">
-                        <h1 class="bg-marron-chocolate text-beige-tostado text-2xl font-bold mb-4">
+                        <!-- <h1 class="bg-marron-chocolate text-beige-tostado text-2xl font-bold mb-4">
                             Resumen de tus productos
-                        </h1>
+                        </h1> -->
                         <!-- canvas para el gráfico --> 
-                        <canvas id="productosChart" width="400" height="200"></canvas>   
+                        <!-- <canvas id="productosChart" width="400" height="200"></canvas>    -->
+
+                        <h1 class="bg-marron-chocolate text-beige-tostado text-5xl text-center font-bold mb-8">
+                            <!-- mb-8 indica el margen entre el h1 y la tabla -->
+                        Histórico de tus productos
+                        </h1>
+
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-oro-antiguo text-s font-medium">
+                                <tr>
+                                    <th class="px-4 py-2 text-left text-marron-chocolate uppercase tracking-wider">
+                                        Producto (fecha)
+                                    </th>
+                                    <th class="px-4 py-2 text-left text-marron-chocolate uppercase tracking-wider">
+                                        Cantidad
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-amber-200 divide-y divide-gray-200">
+                                @foreach($productosCombinados as $producto)
+                                    <tr>
+                                        <td class="px-4 py-2 text-sm text-marron-chocolate">
+                                            {{ $producto['nombre'] }}
+                                        </td>
+                                        <td class="px-4 py-2 text-sm text-marron-chocolate">
+                                            {{ $producto['cantidad'] }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <!--script para renderizar el gráfico -->
+    <!-- script para renderizar el gráfico
     <script>
         //Obtener datos desde Blade con @@json (para convertir el array de php($nombres, $cantidades) en objeto @json de javascript(nombres, cantidades))
         const nombres = @json($nombres);
@@ -49,7 +80,9 @@
                 }
             }
         });
-    </script>
+    </script> -->
+
+    
 
 <!-- Incluir Pie de página -->
 @include('pie.footer')
