e44485c0f17ba2bbadd96ff67ccd71136c6a0e1e
Espe <esmgana70@gmail.com>
2026-01-19 14:58:25 +0100

    Añadir buscador en Catalogo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -38,6 +38,26 @@
     <section class="max-w-7xl mx-auto p-10">
         <h2 class="text-4xl font-extrabold text-center mb-10">🎶 Catálogo de Vinilos</h2>
 
+        <!-- Barra superior con buscador -->
+        <div class="bg-marron-chocolate text-beige-tostado py-3 px-6 flex justify-start items-center shadow-md">
+            
+            <!-- Buscador utilizando un formulario-->
+            <form action="{{ route('productos.buscar') }}" method="GET" class="relative">
+                <input type="text" name="buscar"
+                    placeholder="Buscar por categoría o nombre"
+                    value="{{ request('buscar') }}"
+                    class="px-4 py-2 pr-9 rounded-full bg-crema-suave text-marron-chocolate placeholder-beige-tostado 
+                    border border-marron-chocolate focus:border-oro-antiguo focus:ring-2 focus:ring-oro-antiguo 
+                    transition w-64">
+
+                <!-- Icono lupa -->
+                <button type="submit" 
+                    class="absolute right-3 top-1/2 -translate-y-1/2 text-marron-chocolate">
+                    🔍
+                </button>
+            </form>
+        </div>
+
         {{-- Mostrar texto de búsqueda --}}
         @if(!empty($buscar))
             <h3 class="text-2xl text-center mb-6 text-oro-antiguo">
