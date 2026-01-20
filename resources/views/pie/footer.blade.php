fb8c97682726ba91cd40090d352cce13e23b4e3c
Espe <esmgana70@gmail.com>
2026-01-20 19:46:06 +0100

    Iconos redes sociales

--- resources/views/pie/footer.blade.php
+++ resources/views/pie/footer.blade.php
@@ -8,16 +8,16 @@
             <!-- target="_blank": la tienda permanece abierta y el usuario puede visitar la red social sin perder el sitio (la página de la tienda) 
                 rel="noopener noreferrer": recomendable por seguridad cuando uso target="_blank" -->
             <a href="https://facebook.com" target="_blank" aria-label="Facebook" rel="noopener noreferrer" class="text-2xl hover:text-oro-antiguo transition">   
-                <img src="/icons/Facebook_Logo_Primary.png" alt="Facebook" class="h-6 w-6"/>
+                <img src="/icons/Facebook_Logo_Primary.png" alt="Facebook" class="h-6 w-6" loading="lazy"/>
             </a>
             <a href="https://instagram.com" target="_blank" aria-label="Instagram"> <!-- aria-label es un atributo de accesibilidad para que el sitio sea más inclusivo y accesible -->
-                <img src="/icons/Instagram_icon.png" alt="Instagram" class="h-6 w-6"/>
+                <img src="/icons/Instagram_icon.png" alt="Instagram" class="h-6 w-6" loading="lazy"/>
             </a>
             <a href="https://twitter.com" target="_blank" aria-label="Twitter">
-                <img src="/icons/logotipo-de-twitter.png" alt="Twitter" class="h-6 w-6"/>
+                <img src="/icons/logotipo-de-twitter.png" alt="Twitter" class="h-6 w-6" loading="lazy"/>
             </a>
             <a href="https://tiktok.com" target="_blank" aria-label="TikTok">
-                <img src="/icons/tik-tok.png" alt="TikTok" class="h-6 w-6"/>
+                <img src="/icons/tik-tok.png" alt="TikTok" class="h-6 w-6" loading="lazy"/>
             </a>
         </div>
     </div>
