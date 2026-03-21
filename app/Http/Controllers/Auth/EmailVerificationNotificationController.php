6b5eb71d1c70fb095ca9292eea8384fb50b096f5
Espe <esmgana70@gmail.com>
2026-03-21 21:38:45 +0100

    Comentarios en EmailVerificationNotificationController.php

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
+//Controlador que reenvía el correo de verificación de email para un usuario ya registrado y logueado
 class EmailVerificationNotificationController extends Controller
 {
     /**
@@ -13,12 +14,20 @@ class EmailVerificationNotificationController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
+        //hasVerifiedEmail() comprueba si el usuario ya tiene el email verificado
+        if ($request->user()->hasVerifiedEmail()) 
+        {
+            //Si ya está verificado, lo redirige al dashboard (o a la URL “intended”)
             return redirect()->intended(route('dashboard', absolute: false));
         }
 
+        /*Si no está verificado:
+        Llama a sendEmailVerificationNotification(), que envía un nuevo email con el enlace de 
+        verificación al usuario actual*/
         $request->user()->sendEmailVerificationNotification();
 
+        /*Vuelve a la misma página con un mensaje de estado (por ejemplo, “se ha enviado un nuevo 
+        enlace de verificación”)*/
         return back()->with('status', 'verification-link-sent');
     }
 }
