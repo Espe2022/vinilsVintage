4620b04ea4bad94ecab40fd05ebf90c6af572bd0
Espe <esmgana70@gmail.com>
2026-03-21 22:21:57 +0100

    Comentarios en PasswordController.php

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
+/*Controlador sirve para que un usuario ya logueado cambie su contraseña desde su perfil, 
+no para “olvidé mi contraseña” sino que quiere cambiarla”*/
 class PasswordController extends Controller
 {
     /**
@@ -15,15 +17,24 @@ class PasswordController extends Controller
      */
     public function update(Request $request): RedirectResponse
     {
+        /*Valida los campos y, si hay errores, los guarda en el error bag llamado updatePassword 
+        (útil cuando tienes varios formularios en la misma página)*/
         $validated = $request->validateWithBag('updatePassword', [
+            //Debe coincidir con la contraseña actual del usuario 
             'current_password' => ['required', 'current_password'],
+            /*Debe cumplir las reglas por defecto (Password::defaults())
+            y debe estar confirmada (password_confirmation)*/
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
+        /*Si pasa la validación:
+        Obtiene al usuario actual con $request->user() y actualiza su password en la BD usando 
+        Hash::make*/
         $request->user()->update([
             'password' => Hash::make($validated['password']),
         ]);
 
+        //Vuelve a la misma página con un mensaje de éxito
         return back()->with('status', 'password-updated');
     }
 }
