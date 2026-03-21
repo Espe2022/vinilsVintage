5ca07cf2c555b753571ea689f95d901710083004
Espe <esmgana70@gmail.com>
2026-03-21 21:14:41 +0100

    Comentarios en AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,10 +9,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
+//Controlador que se encarga de mostrar el login, procesar el inicio de sesión y cerrar sesión
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Display the login view.
+     * Muestra la vista del formulario de login (auth.login).
      */
     public function create(): View
     {
@@ -21,27 +23,37 @@ class AuthenticatedSessionController extends Controller
 
     /**
      * Handle an incoming authentication request.
+     * Recibe una LoginRequest (Form Request) que valida email/contraseña y tiene el método authenticate().
+     * 
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        //Intenta loguear al usuario con las credenciales que vienen del formulario
         $request->authenticate();
 
+        //Previene ataques de fijación de sesión cambiando el ID de sesión del usuario por uno nuevo
         $request->session()->regenerate();
 
+        //Redirige al dashboard
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
     /**
      * Destroy an authenticated session.
+     * Cierra la sesión de un usuario, no borra la cuenta del usuario.
      */
     public function destroy(Request $request): RedirectResponse
     {
+        //Hace el logout del usuario actual
         Auth::guard('web')->logout();
 
+        //Invalida toda la sesión 
         $request->session()->invalidate();
 
+        //Genera un nuevo token CSRF por seguridad
         $request->session()->regenerateToken();
 
+        //Redirige a la página de inicio /
         return redirect('/');
     }
 }
