35d8b68bca80d677ae259081b199ae1d6cb9766b
Espe <esmgana70@gmail.com>
2026-03-22 13:43:05 +0100

    Comentarios en PasswordResetLinkController.php

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -8,10 +8,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 
+//Controlador de Laravel para el “Olvidé mi contraseña”
 class PasswordResetLinkController extends Controller
 {
     /**
      * Display the password reset link request view.
+     * Muestra el formulario de “Olvidé mi contraseña” (auth.forgot-password.blade.php), donde 
+     * el usuario introduce su email.
+     * 
      */
     public function create(): View
     {
@@ -25,17 +29,23 @@ class PasswordResetLinkController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        /*Valida que el usuario haya enviado un email válido. Si falla, Laravel vuelve atrás 
+        mostrando el error en el formulario*/
         $request->validate([
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
+        /*Buscar un usuario con ese email.
+        Generar un token temporal de restablecimiento.
+        Enviar un email con un enlace tipo /reset-password?token=... al usuario.*/
         $status = Password::sendResetLink(
             $request->only('email')
         );
 
+        /*Si el email se envió bien (Password::RESET_LINK_SENT), redirige de vuelta mostrando 
+        un mensaje de éxito (por ejemplo “Te hemos enviado el enlace”).
+        Si hubo error (email no encontrado, throttling, etc.), redirige de vuelta con el email 
+        rellenado y un mensaje de error debajo del campo*/
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
