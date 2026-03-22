7b11bedda7f6874c91cbdd265e6f6fbc04742285
Espe <esmgana70@gmail.com>
2026-03-22 18:51:30 +0100

    Comentarios en VerifiEmailController.php

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+//Importa las clases:
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 
+//Controlador de verificación de email
 class VerifyEmailController extends Controller
 {
     /**
@@ -14,14 +16,17 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
+        //Si ya tiene el email verificado, redirige al dashboard
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
         }
 
+        //Llama markEmailAsVerified() en el modelo User y dispara el evento Verified (para logs, notificaciones, etc.)
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
+        //Redirige al dashboard con parámetro ?verified=1
         return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
     }
 }
