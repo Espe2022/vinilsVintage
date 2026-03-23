706f46061ce106a60d34948266cbca083a428363
Espe <esmgana70@gmail.com>
2026-03-23 19:30:06 +0100

    Comentarios en AppLayout.php

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -5,13 +5,21 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
+/**
+ * Componente AppLayout
+ * - Representa la plantilla base de la aplicación
+ * - Se puede usar en Blade como <x-app-layout>
+ */
 class AppLayout extends Component
 {
     /**
-     * Get the view / contents that represents the component.
+     * Devuelve la vista que representa este componente
+     * 
+     * @return \Illuminate\View\View
      */
     public function render(): View
     {
+        //Carga la vista resources/views/layouts/app.blade.php
         return view('layouts.app');
     }
 }
