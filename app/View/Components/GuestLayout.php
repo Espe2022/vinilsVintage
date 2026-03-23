87d23bef97034571696f557f86a28f2b2423dc7c
Espe <esmgana70@gmail.com>
2026-03-23 19:33:57 +0100

    Comentarios en GuestLayout.php

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -5,13 +5,21 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
+/**
+ * Componente GuestLayout
+ * - Representa la plantilla base para usuarios invitados (no logueados)
+ * - Se puede usar en Blade como <x-guest-layout>
+ */
 class GuestLayout extends Component
 {
     /**
-     * Get the view / contents that represents the component.
+     * Devuelve la vista que representa este componente
+     * 
+     * @return \Illuminate\View\View
      */
     public function render(): View
     {
+        //Carga la vista resources/views/layouts/guest.blade.php
         return view('layouts.guest');
     }
 }
