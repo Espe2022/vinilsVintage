9812d3ec46c43e4c32b1c8b48976f6e464ecc716
Espe <esmgana70@gmail.com>
2026-04-04 20:53:34 +0200

    Modificar estilo del Status

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'mb-4 text-xl font-semibold text-marron-chocolate bg-oro-antiguo/40 px-4 py-2 rounded-md']) }}>
+    <div {{ $attributes->merge(['class' => 'mb-4 inline-block mx-auto text-lg font-semibold text-marron-chocolate bg-[#f5efe3] border border-[#c9a96b] border-l-[5px] border-l-[#b08d57] rounded-lg shadow-sm px-4 py-2']) }}>
         {{ $status }}
     </div>
 @endif
